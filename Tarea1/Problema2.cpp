2962df97e2302c0aba6fe918e3d9383c0048913c
CharlesLakes <carloslagoscortes@hotmail.com>
2021-05-09 15:09:52 -0400

    Añadiendo comentarios

--- Tarea1/Problema2.cpp
+++ Tarea1/Problema2.cpp
@@ -7,24 +7,72 @@ class A{
     private:
         int *B;
         unsigned long n;
-        unsigned long k;
+        unsigned long b;
     public:
         A(){
             n = 0;
-            k = 0;
-            B = new int[(int) pow(2,k)];
+            b = 1;
+            B = new int[b];
         };
+        ~A(){
+            delete[] B;
+        }
         bool setValue(unsigned long i,int v);
         int getValue(unsigned long i);
         void append(int v);
         void remove();
         unsigned long size();
-        /*unsigned long size_k();*/
 };
+
+
+
+/*****
+* void A::setValue
+******
+* Cambia el valor de un elemento de el arreglo solo si existe
+******
+* Input: 
+* unsigned long i: La posición que quieres asignar el valor
+* int v: El valor que quieres introducir en el arreglo
+******
+* Returns:
+* bool, retorna True si se pudo asignar y False si no tuvo ningun efecto en el arreglo
+*****/
+bool A::setValue(unsigned long i,int v){
+
+};
+
+/*****
+* int A::getValue
+******
+* Obtiene el valor de una posición de el arreglo
+******
+* Input: 
+* unsigned long i: La posición de el valor que quieres obtener
+******
+* Returns:
+* int, El valor que quieres obtener
+*****/
+int A::getValue(unsigned long i){
+
+}
+
+
+/*****
+* void A::append
+******
+* Agrega un elemento al final arregla gestionando el espacio de memoria
+******
+* Input:
+* int v: El valor el cual se quiere ingresar al arreglo
+******
+* Returns:
+* void, No tiene un retorno
+*****/
 void A::append(int v){
-    if((unsigned long) pow(2,k) == n){
-        k++;
-        int *temp = new int[(int) pow(2,k)];
+    if(b == n){
+        b *= 2;
+        int *temp = new int[b];
         for(unsigned long i = 0; i < n; i++)
             temp[i] = B[i];
         delete[] B;
@@ -34,12 +82,23 @@ void A::append(int v){
     B[n] = v;
     n++;
 };
+
+/*****
+* void A::remove
+******
+* Elimina el ultimo elemente de el arreglo gestionando el uso de memoria
+******
+* Input: Sin entradas
+******
+* Returns:
+* void, No tiene un retorno
+*****/
 void A::remove(){
     if(n == 0) return;
-    if((unsigned long) pow(2,k - 1) + 1 == n ){
-        if(k > 0)
-            k--;
-        int *temp = new int[(int) pow(2,k)];
+    if( ((unsigned long) b/2) + 1 == n && n > 1){
+        if(b > 1)
+            b /= 2;
+        int *temp = new int[b];
         for(long unsigned i = 0; i < n; i++)
             temp[i] = B[i];
         delete[] B;
@@ -48,11 +107,19 @@ void A::remove(){
     }
     n--;
 }
+
+/*****
+* void A::size
+******
+* Obtiene la cantidad de elementos de el arreglo
+******
+* Input: Sin entradas
+******
+* Returns:
+* unsigned long, Retorna la cantidad de elementos de el arreglo
+*****/
+
 unsigned long A::size(){
     return n;
 };
-/*
-unsigned long A::size_k(){
-    return pow(2,k);
-}*/
 
