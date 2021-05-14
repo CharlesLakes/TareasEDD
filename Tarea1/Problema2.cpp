3768577d07c82b121e9eebd1b887363129c44025
CharlesLakes <carloslagoscortes@hotmail.com>
2021-05-14 16:24:02 -0400

    Final Tarea 1

--- Tarea1/Problema2.cpp
+++ Tarea1/Problema2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -39,8 +38,12 @@ class A{
 * bool, retorna True si se pudo asignar y False si no tuvo ningun efecto en el arreglo
 *****/
 bool A::setValue(unsigned long i,int v){
-
-};
+    if(i >= n){
+        return false;
+    }
+    B[i] = v;
+    return true;
+}
 
 /*****
 * int A::getValue
@@ -54,9 +57,12 @@ bool A::setValue(unsigned long i,int v){
 * int, El valor que quieres obtener
 *****/
 int A::getValue(unsigned long i){
-
+    if(i >= n){
+        cerr << "Error de acceso al arreglo" << endl;
+        exit(1);
+    }
+    return B[i];
 }
-
 
 /*****
 * void A::append
@@ -123,3 +129,39 @@ unsigned long A::size(){
     return n;
 };
 
+int main(){
+    cout << "Creando arreglo..." << endl;
+    A arreglo;
+
+    cout << "Agregando 10 y 15" << endl;
+    arreglo.append(10);
+    arreglo.append(15);
+
+
+    cout << "Largo: " << arreglo.size() << endl;
+
+    cout << "Asignando 5 en la posicion 0" << endl;
+    arreglo.setValue(0,5);
+
+    cout << "Valor de la posicion 0: " << arreglo.getValue(0) << endl;
+
+    cout << "Agregando un -5 al final de el arreglo" << endl;
+    arreglo.append(-5);
+
+    cout << endl;
+    cout << "Elementos de el arreglo: " << endl;
+    for(int i= 0 ; (unsigned long) i < arreglo.size(); i++){
+        cout << arreglo.getValue(i) << endl;
+    }
+    cout << endl;
+    cout << "Removiendo el ultimo elemento" << endl;
+    arreglo.remove();
+    cout << "Elementos de el arreglo: " << endl;
+
+    for(int i= 0 ; (unsigned long) i < arreglo.size(); i++){
+        cout << arreglo.getValue(i) << endl;
+    }
+
+
+
+}
