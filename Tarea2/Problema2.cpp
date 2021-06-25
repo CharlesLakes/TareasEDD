48f764b8e35bd7931c5a4d79baa132b943d38998
CharlesLakes <46362356+CharlesLakes@users.noreply.github.com>
2021-06-25 18:19:26 -0400

    final

--- Tarea2/Problema2.cpp
+++ Tarea2/Problema2.cpp
@@ -10,6 +10,18 @@ struct tNodo{
     tNodo* der;
 };
 
+/*****
+* class tPolinomio
+******
+* Clase polinomio
+******
+* Input: 
+* Sin entradas
+******
+* Returns:
+* No tiene un retorno
+*****/
+
 class tPolinomio{
     private:
         tNodo *Padre;
@@ -24,13 +36,53 @@ class tPolinomio{
         
 };
 
+/*****
+* tPolinomio::tPolinomio
+******
+* Se define el constructor de la clase
+******
+* Input: 
+* Sin entradas
+******
+* Returns:
+* No tiene un retorno
+*****/
 
 tPolinomio::tPolinomio(){
     n = 0;
 }
+
+/*****
+* tPolinomio::~tPolinomio
+******
+* Limpia o reinicializa la lista
+******
+* Input: 
+* Sin entradas
+******
+* Returns:
+* No tiene un retorno
+*****/
+
 tPolinomio::~tPolinomio(){
     clear();
 }
+
+
+/*****
+* void coefHelp()
+******
+* Funcion recursiva que a partir de un nodo padre busca el coeficiente de un exponente determinado, el cual es devuelto a partir del puntero i
+******
+* Input:
+* tNodo* temp: puntero a nodo
+* unsigned int iE: entrada del exponente
+* int *i: puntero a la respuesta
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
 
 void coefHelp(tNodo* temp,unsigned int iE,int *i){
     if(temp == NULL) return;
@@ -41,11 +93,37 @@ void coefHelp(tNodo* temp,unsigned int iE,int *i){
     };
 };
 
+/*****
+* int tPolinomio::coeficiente()
+******
+* Obtiene el coeficiente del monomio
+******
+* Input:
+* unsigned int iE: entrada del exponente
+* .......
+******
+* Returns:
+* int i, que contiene la respuesta (coeficiente)
+*****/
+
 int tPolinomio::coeficiente(unsigned int iE){
     int i;
     coefHelp(Padre,iE,&i);
     return i;
 };
+
+/*****
+* void clearHelp()
+******
+* funcion recursiva, la cual reinicializa el arbol
+******
+* Input:
+* tNodo* temp: puntero a nodo
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
 
 void clearHelp(tNodo* temp){
     if(temp == NULL) return;
@@ -54,9 +132,36 @@ void clearHelp(tNodo* temp){
     delete temp;
 };
 
+/*****
+* void tPolinomio::clear()
+******
+* reinicializa desde el nodo Padre
+******
+* Input:
+* Sin parámetros
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
+
 void tPolinomio::clear(){
     clearHelp(Padre);
 };
+
+/*****
+* void insertHelp
+******
+* funcion recursiva que inserta un valor al nodo
+******
+* Input:
+* tNodo* nuevo: nuevo puntero
+* tNodo* temp: puntero a nodo
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
 
 void insertHelp(tNodo* nuevo, tNodo* temp){
     if(temp->E > nuevo->E){
@@ -74,6 +179,20 @@ void insertHelp(tNodo* nuevo, tNodo* temp){
     };
 };
 
+/*****
+* void tPolinomio::insert()
+******
+* inserta un valor al nodo
+******
+* Input:
+* unsigned int iE: entrada del exponente
+* int iC: entrada del coeficiente
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
+
 void tPolinomio::insert(unsigned int iE, int iC){
     tNodo *nuevo = new tNodo;
     nuevo->E = iE;
@@ -87,7 +206,22 @@ void tPolinomio::insert(unsigned int iE, int iC){
 }
 
 
-
+/*****
+* void inOrdenInverso
+******
+* funcion recursiva que recorre el arbol en in-orden
+******
+* Input:
+* tNodo* nodo: nodo temporal
+* float x: x evaluado 
+* unsigned int Emin: exponente minimo
+* float* resultado: direccion de memoria en que se guarda el resultado
+* unsigned int* temp: guarda el E temporal de los nodos
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
 
 void inOrdenInverso(tNodo* nodo, float x ,unsigned int Emin,float* resultado,unsigned int* temp) {
     if (nodo == NULL) return;
@@ -108,7 +242,18 @@ void inOrdenInverso(tNodo* nodo, float x ,unsigned int Emin,float* resultado,uns
     }
 }
 
-// recorrido in-orden de un ABB T
+/*****
+* float tPolinomio::evaluar()
+******
+* Evalua segun x en el polinomio
+******
+* float x: numero por el cual se reemplaza la x
+* .......
+******
+* Returns:
+* TipoRetorno, Descripción retorno
+*****/
+
 float tPolinomio::evaluar(float x) {
     if(Padre == NULL) return 0;
 
@@ -128,6 +273,20 @@ float tPolinomio::evaluar(float x) {
 }
 
 
+/*****
+* tPolinomio* ObtenerPolinomios()
+******
+* Interpreta y lee los datos de el archivo .txt y los guarda en una lista
+******
+* Input:
+* string name_file: nombre del archivo de texto
+* int& pos : Posición de el cursor al leer
+* int& length : Variable por referencia que guardara el largo 
+* .......
+******
+* Returns:
+* listapolinomios, lista de tipo tPolinomio que contiene los polinomios
+*****/
 
 
 tPolinomio* ObtenerPolinomios(string name_file ,int& pos,int& length){
@@ -161,6 +320,24 @@ tPolinomio* ObtenerPolinomios(string name_file ,int& pos,int& length){
     fp.close();
     return listapolinomios;
 }
+
+/*****
+* void InterpretarOperaciones()
+******
+* Lee el archivo con los polinomios, busca el desado,
+* evalua, obtiene el coficiente y finalmente lo escribe en otro archivo
+******
+* Input:
+* string input_file : Nombre de el archivo que se lee
+* string output_file : Nombre de el archivo donde se escribe
+* tPolinomio polinomios[] : Lista que contiene los polinomios
+* int& length : Variable por referencia que guardara el largo
+* int pos = 0 : Posición de el cursor al escribir
+* .......
+******
+* Returns:
+* void, sin retorno
+*****/
 
 void InterpretarOperaciones(string input_file,string output_file,tPolinomio polinomios[],int lenght,int pos = 0){
     fstream in;
